322a4a6aad9b7b078bf7a629b8cc16e4d5a18a08
ForkingPaths2040 <amberm@smallwoodhome.com>
2021-01-15 15:50:46 -0600

    styling cards

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import './ItemList.css'
 import ItemCard from './ItemCard'
 
 class ItemList extends Component {
   render() {
     return (
-      <div>
-        <ul>
+      <div className="list-container">
           {this.props.list.map((task, index) => {
             return <ItemCard key={`${index}_${task.name}`} item={task} />;
           })}
-        </ul>
       </div>
     );
   }
